a78e693d20c949ee60a834b0bb6da35d71dff7e1
Silvio Calixto <silvio.calixto.sc@gmail.com>
2026-03-12 17:19:28 -0300

    primeiro commit

---
+++ model/cadastro.php
@@ -0,0 +1,42 @@
+<?php 
+  namespace OrganizacaoTarefas\model;
+
+
+  class Cadastro {
+
+
+    private int $codigo;
+    private string $nome;
+    private string $email;
+    private string $senha;
+
+
+
+    public function __construct( int $codigo,
+                                string $nome,
+                                string $email,
+                                string $senha) {
+
+            $this->codigo = $codigo;
+            $this->nome   = $nome;
+            $this->email  = $email;
+            $this->senha  = $senha;
+     }
+
+     public function __get(string $dado): mixed {
+      return $this->dado;
+     }
+
+     public function __set(string $variavel, string $novoDado): void {
+      $this->variavel = $novoDado;
+     }
+
+     public function imprimir(): string {
+        return "<br>Código: ".$this->codigo.
+               "<br>Nome: ".$this->nome.
+               "<br>E-mail: ".$this->email.
+               "<br>Senha: ".$this->senha;
+     }
+  }
+
+?>
